1df891f7530ba8e9d544b7fd6ac24e289219de2d
Jean Mendes <jeanmds7809@gmail.com>
2023-10-26 14:57:21 -0300

    recebendo parâmetros na rota

--- node_express/index.js
+++ node_express/index.js
@@ -6,8 +6,16 @@ const app  = express()
 
 const caminhoBase = path.join(__dirname, "templates")
 
+app.get('/usuarios/:id', (require, response) => {
+    const id = require.params.id
+
+    console.log(`acessando dados do usuário ${id}`)
+
+    response.sendFile(`${caminhoBase}/usuarios.html`)
+})
+
 app.get('/', (require, response) => {
-    response.sendFile(`${caminhoBase}/`)
+    response.sendFile(`${caminhoBase}/index.html`)
 })
 
 app.listen(3000, () => {
